1e3e0f64b6dd0a71f9725219e595470bcaacc467
Davidson Reuben <rubyrailseng86@gmail.com>
2014-02-28 17:28:00 -0500

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-alert('Hey find me to know where to put the javascript');
+// Generate a random number
+var random_number = Math.floor(Math.random() * 100);
+
+// Get the user's name
+var name = prompt("What is your name?");
+
+// Ask user to enter a number
+var guess = prompt("Hi " + name + ". Enter a number between 0 and 100")
+
+// Tell user whether they won or lost
+if(guess == random_number) {
+  alert("You win!");
+} else {
+  alert("You lose!");
+}
+
